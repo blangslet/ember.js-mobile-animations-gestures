b852e370389b44ccbdd466713410e88ee94e8121
Bryan Langslet <blangslet@me.com>
2013-07-16 01:27:59 -0700

    specified ember production variant

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ App::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.ember.variant = :production
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
