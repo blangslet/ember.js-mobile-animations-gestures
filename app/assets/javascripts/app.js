60deb979c6f78f46c030bc847271f9a4aeacce5f
Bryan Langslet <blangslet@me.com>
2013-07-16 01:30:27 -0700

    removed empty directory from manifest

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,7 +2,6 @@
 #= require_tree ./models
 #= require_tree ./controllers
 #= require_tree ./views
-#= require_tree ./helpers
 #= require_tree ./templates
 #= require_tree ./routes
 #= require ./router
